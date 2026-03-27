4340b3a0132211780b8496fc7d7e276482b6b734
a8v0r6 <a8v0r6@protonmail.com>
2026-03-27 15:13:07 +0530

    project structure

---
+++ prompt_ui.py
@@ -0,0 +1,7 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_core.prompts import PromptTemplate,load_prompt
+
+load_dotenv()
+model = ChatOpenAI()
